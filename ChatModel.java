c066f92071e2b287caf962c5296fe16fa88e8a23
Maja <maja.jablonska.yyy@gmail.com>
2018-03-11 21:56:46 +0100

    First Commit

--- ChatModel.java
+++ ChatModel.java
@@ -1,6 +1,7 @@
 import java.util.ArrayList;
 public class ChatModel
 {
+	protected static int maxId = 0;
     private class Message
     {
         private String text;
@@ -15,8 +16,39 @@ public class ChatModel
             return this.text;
         }
     }
-
-    private ArrayList<Message> messages;
+	
+	private class User
+	{
+		private int id;
+		private String username;
+		public User(String username)
+		{
+			this.username = username;
+			maxId++;
+		}
+		public void SendMessage()
+		{
+		}
+	}
+	
+	private class Host extends User
+	{
+		public Host(String username)
+		{
+			super(username);
+		}
+	}
+	
+	private class Client extends User
+	{
+		public Client(String username)
+		{
+			super(username);
+		}
+	}
+	
+	private ArrayList<Message> messages;
+    
     ChatModel()
     {
         messages = new ArrayList<Message>();
